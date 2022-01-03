bfe63f1e2bedef96baf99cda916e4d364827086d
novailoveyou <novailoveyoux@gmail.com>
2022-01-03 18:16:06 +0300

    file based routing

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,6 +2,10 @@ const Home = () => {
   return (
     <div>
       <h1>Welcome</h1>
+      <p>
+        Remix is a full stack web framework by the creators of React Router.
+        This is a simple blog app from the Traversy Media Remix crash course.
+      </p>
     </div>
   )
 }
---
+++ app/routes/posts.tsx
@@ -0,0 +1,9 @@
+const Posts = () => {
+  return (
+    <div>
+      <h1>This is the posts route</h1>
+    </div>
+  )
+}
+
+export default Posts
